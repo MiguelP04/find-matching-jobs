27a95d944be1f3191c84e30bc4743e97b98d66ba
joseruizvene <joseleonardoruizgoitia@gmail.com>
2026-05-02 20:02:12 -0400

    feat: implement AppService with a greeting method

--- apps/backend/src/app.service.ts
+++ apps/backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'hola soy neutro shorty y me gusta el pito!';
   }
 }
